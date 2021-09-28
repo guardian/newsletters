77ec48b5e164d4de85fb26c327114f260b070080
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-09-28 11:29:48 +0100

    chore: Added @guardian/prettier config

--- src/pages/api/newsletters.js
+++ src/pages/api/newsletters.js
@@ -1,29 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
 
 /**
  * @param {NextApiRequest} _
  * @param {NextApiResponse} res
  */
 export default function handler(_, res) {
-  res.status(200).json(getSheetsNewsletters());
+	res.status(200).json(getSheetsNewsletters());
 }
 
 const getSheetsNewsletters = () => {
-  return [
-    {
-      pillar: "FEATURES",
-      email: "The Upside",
-      previews: "https://www.theguardian.com/",
-      topic: "Series of same name",
-      frequency: "Weekly, Friday/ad usually, ~12-2pm",
-      format: "Article",
-      contact: "A contact",
-      opha_alert: undefined,
-      sign_up_page:
-        "https://www.theguardian.com/world/2018/feb/12/the-upside-sign-up-for-our-weekly-email",
-      notes: undefined,
-      treat: "world/series/the-upside",
-    },
-  ];
+	return [
+		{
+			pillar: 'FEATURES',
+			email: 'The Upside',
+			previews: 'https://www.theguardian.com/',
+			topic: 'Series of same name',
+			frequency: 'Weekly, Friday/ad usually, ~12-2pm',
+			format: 'Article',
+			contact: 'A contact',
+			opha_alert: undefined,
+			sign_up_page:
+				'https://www.theguardian.com/world/2018/feb/12/the-upside-sign-up-for-our-weekly-email',
+			notes: undefined,
+			treat: 'world/series/the-upside',
+		},
+	];
 };
--- src/types/newsletters.ts
+++ src/types/newsletters.ts
@@ -1,13 +1,13 @@
 export type Newsletter = {
-  pillar: string;
-  email: string;
-  previews: string;
-  topic: string;
-  frequency: string;
-  format: "Article";
-  contact: string;
-  opha_alert: undefined;
-  sign_up_page: string;
-  notes: undefined;
-  treat: string;
+	pillar: string;
+	email: string;
+	previews: string;
+	topic: string;
+	frequency: string;
+	format: 'Article';
+	contact: string;
+	opha_alert: undefined;
+	sign_up_page: string;
+	notes: undefined;
+	treat: string;
 };
