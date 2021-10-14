7610c537a44c26895fb28af6058a69241b7c456e
Nasser Dahmani <nasser.dahmani.freelancer@guardian.co.uk>
2021-10-14 11:37:07 +0100

    Added dotenv-flow

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,16 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import awsServerlessExpress from 'aws-serverless-express';
 import express from 'express';
 import { getNewsletters } from './api/newsletters';
+import * as dotenvFlow from 'dotenv-flow';
 
+dotenvFlow.config();
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 
 app.get('/newsletters', getNewsletters);
 
 // If local then don't wrap in serverless
-const PORT = 3000;
+const PORT = process.env.PORT;
 if (process.env.NODE_ENV === 'development') {
 	app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 } else {
