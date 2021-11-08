3cd46505cddffd1e11de30c8bc2e9d4beadd07a3
George Buckingham <george.buckingham@guardian.co.uk>
2021-11-08 09:35:57 +0000

    Proof of concept showing Google auth without json file on filesystem

--- src/lib/googleNewsletterSheets.ts
+++ src/lib/googleNewsletterSheets.ts
@@ -10,6 +10,10 @@ const THEME_INDEX = 13;
 
 const readNewslettersSheet = async (): Promise<sheetsV4.Schema$RowData[]> => {
 	const googleAuth = new google.auth.GoogleAuth({
+		credentials: {
+			client_email: process.env.GOOGLE_CLIENT_EMAIL,
+			private_key: process.env.GOOGLE_PRIVATE_KEY,
+		},
 		scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 	});
 	const auth = await googleAuth.getClient();
