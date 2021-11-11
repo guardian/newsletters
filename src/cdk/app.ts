f22cf4fd6fc884a10740af26b1a43dc2dc6726f3
Nasser Dahmani <nasser.dahmani.freelancer@guardian.co.uk>
2021-11-11 13:44:38 +0000

    add ssm permissions

--- src/cdk/app.ts
+++ src/cdk/app.ts
@@ -1,19 +1,30 @@
 import apigateway = require('@aws-cdk/aws-apigateway');
+import * as iam from '@aws-cdk/aws-iam';
 import lambda = require('@aws-cdk/aws-lambda');
 import s3 = require('@aws-cdk/aws-s3');
 import cdk = require('@aws-cdk/core');
 
 export class LambdaService extends cdk.Stack {
 	constructor(scope: cdk.Construct, id: string) {
-		super(scope, id, { env: { region: 'eu-west-1' } });
+		const region = 'eu-west-1';
+		super(scope, id, { env: { region } });
 
 		const stage = new cdk.CfnParameter(this, 'Stage', {
 			type: 'String',
 			default: 'CODE',
 		});
 
+		const ssmPolicy = new iam.PolicyStatement({
+			actions: [
+				'ssm:GetParameters',
+				'ssm:GetParameter',
+				'ssm:GetParametersByPath',
+			],
+			resources: [`*`],
+		});
+
 		const bucket = 'aws-frontend-artifacts';
-		const key = `frontend/${stage.value}/lambda/newsletters-api.zip`;
+		const key = `frontend/${stage.value}/newsletters-api/newsletters-api.zip`;
 
 		const handler = new lambda.Function(this, 'frontend-newsletters-api', {
 			runtime: lambda.Runtime.NODEJS_14_X,
@@ -24,6 +35,12 @@ export class LambdaService extends cdk.Stack {
 			handler: 'server.handler',
 			functionName: `frontend-newsletters-api-${stage.value}`,
 		});
+
+		handler.role?.attachInlinePolicy(
+			new iam.Policy(this, 'ssm-parameters-policy', {
+				statements: [ssmPolicy],
+			}),
+		);
 
 		// tslint:disable-next-line: no-unused-expression
 		new apigateway.LambdaRestApi(this, 'newsletters-api', {
