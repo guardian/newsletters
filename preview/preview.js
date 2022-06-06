8b960c4c295e223bb0f56c490a2e2500d04448ce
David Blatcher <dv.bltchr@gmail.com>
2022-06-06 10:35:35 +0100

    remove sample HTML file

--- preview/preview.js
+++
@@ -1,6 +0,0 @@
-const feedback = document.querySelector('#feedback');
-const dataScript = document.querySelector('#data');
-const data = JSON.parse(dataScript.textContent);
-
-feedback.innerHTML += '<p>RUNNING SCRIPT</p>';
-feedback.innerHTML += `<p>There are ${data.length} newsletters</p>`;
--- preview/write.ts
+++ preview/write.ts
@@ -10,16 +10,32 @@ import { parseStringifiedCSV } from '../src/util/csv';
 const USE_LIVE_DATA = !!process.env.USE_LIVE_DATA;
 console.log({ USE_LIVE_DATA });
 
+const logFeedback = (
+	versionNumber: string,
+	newsletters: EmailNewsletter[],
+): void => {
+	console.log({ versionNumber });
+	console.log('INDEX\t MATCH\t NAME');
+	newsletters.forEach((newsletter, index) => {
+		console.log(
+			index,
+			'\t',
+			EmailNewsletterType.is(newsletter),
+			'\t',
+			newsletter.name,
+		);
+	});
+};
+
 const getNewslettersFromLocalCsv = async (): Promise<EmailNewsletter[]> => {
 	const csvData = await readFileSync('./preview/sampleData.csv').toString();
 	const cellsInRows = parseStringifiedCSV(csvData);
-	const versionNumber = cellsInRows[0][0];
-	const newsletters = cellsInRows.slice(1).map(rowToNewsletter);
+	const unverifiedNewsletters = cellsInRows.slice(1).map(rowToNewsletter);
 
 	let lastGroup = '_NO_GROUP_';
 	let lastTheme = '_NO_Theme_';
 
-	newsletters.forEach((newsletter) => {
+	unverifiedNewsletters.forEach((newsletter) => {
 		if (newsletter.group) {
 			lastGroup = newsletter.group;
 		} else {
@@ -32,33 +48,17 @@ const getNewslettersFromLocalCsv = async (): Promise<EmailNewsletter[]> => {
 		}
 	});
 
-	console.log({ versionNumber });
-	console.log('INDEX\t MATCH\t NAME');
-	newsletters.forEach((newsletter, index) => {
-		console.log(
-			index,
-			'\t',
-			EmailNewsletterType.is(newsletter),
-			'\t',
-			newsletter.name,
-		);
-	});
-
-	return newsletters.filter(EmailNewsletterType.is);
+	logFeedback(cellsInRows[0][0], unverifiedNewsletters);
+	return unverifiedNewsletters.filter(EmailNewsletterType.is);
 };
 
-const populateTemplate = async (): Promise<void> => {
+const writePreviewJson = async (): Promise<void> => {
 	const data = USE_LIVE_DATA
 		? await getEmailNewsletters()
 		: await getNewslettersFromLocalCsv();
 	const dataString = JSON.stringify(data);
-	const template = await readFileSync(
-		'./preview/_previewTemplate.html',
-	).toString();
 
-	const output = template.replace('<!-- DATA PLACEHOLDER -->', dataString);
-	await writeFileSync('./preview/preview.html', output);
 	await writeFileSync('./preview/preview.json', dataString);
 };
 
-populateTemplate();
+writePreviewJson();
