45b8f59117fe2ea8f2c5b8ba38ec7e800656a54d
David Blatcher <dv.bltchr@gmail.com>
2022-06-15 14:38:00 +0100

    add includeCancelled argument to buildNewsletters

--- src/jobs/jobs.ts
+++ src/jobs/jobs.ts
@@ -1,14 +1,27 @@
 import { ALBResult } from 'aws-lambda';
 import { NEWSLETTERS_BUCKET_NAME } from '../constants';
 import { s3upload } from '../lib/s3Upload';
-import { getEmailNewsletters } from './newsletters';
+import {
+	getEmailNewsletters,
+	getEmailNewslettersIncludingCancelled,
+} from './newsletters';
 
-const buildNewsletters = async (): Promise<ALBResult> => {
+const buildNewsletters = async (
+	includeCancelled = false,
+): Promise<ALBResult> => {
 	try {
-		const newsletters = await getEmailNewsletters();
+		const newsletters = includeCancelled
+			? await getEmailNewslettersIncludingCancelled
+			: await getEmailNewsletters();
+
+		// TO DO - check if this is all that is required to upload a second document to the bucket
+		const keySuffix = includeCancelled
+			? 'newsletters-cancelled'
+			: 'newsletters';
+
 		await s3upload({
 			Bucket: NEWSLETTERS_BUCKET_NAME,
-			Key: `${process.env.STAGE}/newsletters`,
+			Key: `${process.env.STAGE}/${keySuffix}`,
 			Body: JSON.stringify(newsletters),
 		});
 		return {
