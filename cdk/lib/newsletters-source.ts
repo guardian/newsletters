6ba6faf0efeb745f20b187e44b48582982d2ea5e
George B <705427+georgeblahblah@users.noreply.github.com>
2022-06-30 15:33:31 +0100

    Remove unused imports

--- cdk/lib/newsletters-source.ts
+++ cdk/lib/newsletters-source.ts
@@ -1,8 +1,6 @@
-import { join } from 'path';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import type { App } from 'aws-cdk-lib';
-import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
 
 export class NewslettersSource extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
