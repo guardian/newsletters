b2a2ecadc478606821f6eee713e6ea779c25bf36
George B <705427+georgeblahblah@users.noreply.github.com>
2022-07-01 11:15:13 +0100

    Update app names

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,11 +3,11 @@ import { App } from 'aws-cdk-lib';
 import { NewslettersSource } from '../lib/newsletters-source';
 
 const app = new App();
-new NewslettersSource(app, 'NewslettersSource-CODE', {
+new NewslettersSource(app, 'newsletters-source-CODE', {
 	stack: 'newsletters',
 	stage: 'CODE',
 });
-new NewslettersSource(app, 'NewslettersSource-PROD', {
+new NewslettersSource(app, 'newsletters-source-PROD', {
 	stack: 'newsletters',
 	stage: 'PROD',
 });
